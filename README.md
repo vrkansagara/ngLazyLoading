4b9474fb56a457754fe6909fe21a0bd1189f3a12
Vallabh Kansagara <vrkansagara@gmail.com>
2018-07-19 14:15:59 +0530

    Adding Feature Modules

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ngLazyLoading
+ 
+ Ref:-https://angularfirebase.com/lessons/how-to-lazy-load-components-in-angular-4-in-three-steps/
+
+ng new lazyDemo --routing
+ng g module lazy --flat
+ng g component lazy-parent --module lazy
+ng g component lazy-child --module lazy
---
+++ src/app/customer-dashboard/customer-dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CustomerDashboardComponent],
+  exports: [
+    CustomerDashboardComponent
+  ]
+})
+export class CustomerDashboardModule { }
